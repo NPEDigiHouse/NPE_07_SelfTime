e034a1b92f313a3b775ca2c638613cd24c2e0054
さん <phantom26isn@gmail.com>
2021-05-29 23:55:37 +0800

    add

--- app/src/main/java/com/example/selftimeapp/activities/LoginActivity.java
+++ app/src/main/java/com/example/selftimeapp/activities/LoginActivity.java
@@ -1,21 +1,102 @@
 package com.example.selftimeapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.selftimeapp.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView etEmail, etPassword;
+    private Button btnLogin, btnDaftar;
+    private FirebaseAuth mAuth;
+    private  FirebaseAuth.AuthStateListener authStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        btnDaftar = findViewById(R.id.toBuatAkun);
+        etEmail =  findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.login);
+
+        btnDaftar.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser u = LoginActivity.this.mAuth.getCurrentUser();
+                if (u != null) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+            }
+        };
+    }
+
+    private void login() {
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+
+        if (email.isEmpty()){
+            etEmail.setError("Email is required");
+            etEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            etEmail.setError("please provide valid email");
+            etEmail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            etPassword.setError("Password is required");
+            etPassword.requestFocus();
+        }
+        if (password.length() < 6){
+            etPassword.setError("password must be 6 character");
+            etPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        if (task.isSuccessful()){
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        }else {
+                            Toast.makeText(LoginActivity.this, "failed login", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login:
+                login();
+                break;
+            case R.id.toBuatAkun:
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+                finish();
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/selftimeapp/activities/SplashActivity.java
+++ app/src/main/java/com/example/selftimeapp/activities/SplashActivity.java
@@ -15,18 +15,9 @@ public class SplashActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
-
-        // set timer for 2 seconds
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-//                String userId = Util.getUserIdLocal(SplashScreen.this);
-//                Intent intent;
-//                if (userId.isEmpty()) {
-//                    intent = new Intent(SplashActivity.this, LoginActivity.class);
-//                } else {
-//                    intent = new Intent(SplashActivity.this, MainActivity.class);
-//                }
                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                 // start activity
                 startActivity(intent);
