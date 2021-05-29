a2296a3e752531d46bcfaaf669f0d071e19a57a8
muammarahlnn <21muammarahlan@gmail.com>
2021-05-29 23:42:37 +0800

    ADD: Delete plan

--- app/src/main/java/com/example/selftimeapp/activities/ListPlanActivity.java
+++ app/src/main/java/com/example/selftimeapp/activities/ListPlanActivity.java
@@ -1,15 +1,22 @@
 package com.example.selftimeapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.selftimeapp.R;
 import com.example.selftimeapp.adapters.MyNoteAdapter;
 import com.example.selftimeapp.models.Note;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -47,6 +54,30 @@ public class ListPlanActivity extends AppCompatActivity implements MyNoteAdapter
 
     @Override
     public void btnDeleteClicked(int position) {
-
+        // get note reference
+        DatabaseReference notesRef = FirebaseDatabase.getInstance().getReference()
+                .child("notes");
+        notesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int pos = 0;
+                for (DataSnapshot data : snapshot.getChildren()) {
+                    if (pos == position) {
+                        // delete value
+                        if (data.getKey() != null) {
+                            notesRef.child(data.getKey()).removeValue();
+                        }
+                    }
+                    pos++;
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ListPlanActivity.this, "Terjadi kesalahan pada database.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Toast.makeText(this, "Note berhasil dihapus", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(ListPlanActivity.this, ListPlanActivity.class));
+        finish();
     }
 }
